a31cfffc137a64a23069b2474d68bac77b39eccf
f4exb <f4exb06@gmail.com>
2017-07-28 22:43:00 +0200

    use setup.py. New formatting with date in French

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='papiclock',
+      version='0.0.1',
+      description="PaPiRus My Clock",
+      author='f4exb',
+      author_email='f4exb06@gmail.com',
+      url='https://github.com/f4exb',
+      packages=['papiClock'],
+      scripts=['papiclock'],
+      data_files=[]
+     )
