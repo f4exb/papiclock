bbd66b991bf666ff6f381fd101fc45f4e40d5a76
f4exb <f4exb06@gmail.com>
2017-12-17 00:48:38 +0100

    Added a script for TFT screens

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='papiclock',
-      version='0.0.1',
+      version='0.0.2',
       description="PaPiRus My Clock",
       author='f4exb',
       author_email='f4exb06@gmail.com',
       url='https://github.com/f4exb',
       packages=['papiClock'],
-      scripts=['papiclock'],
+      scripts=['papiclock', 'tftclock'],
       data_files=[]
      )
