b4d1daa9c21c8abb391ab0b51bc0cdb9d8ae922d
f4exb <f4exb06@gmail.com>
2018-04-08 14:33:47 +0200

    New API entry to return only run information

--- papiClock/apiserver.py
+++ papiClock/apiserver.py
@@ -12,7 +12,8 @@ meteo_dict = {
 }
 
 urls = (
-    '/api/meteo', 'web_process_meteo'
+    '/api/meteo', 'web_process_meteo',
+    '/api/meteo/run', 'web_process_run'
 )
 
 class MyApp(web.application):
@@ -27,6 +28,19 @@ class web_process_meteo:
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Access-Control-Allow-Credentials', 'true')
         output = json.dumps(meteo_dict)
+        return output
+
+class web_process_run:
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Access-Control-Allow-Credentials', 'true')
+        meteo_run_dict = {}
+        meteo_run_dict["update_ts"] = meteo_dict["update_ts"]
+        meteo_run_dict["run_number"] = meteo_dict["run_number"]
+        meteo_run_dict["geoloc"] = {}
+        meteo_run_dict["geoloc"]["lat"] = meteo_dict["geoloc"]["lat"]
+        meteo_run_dict["geoloc"]["lon"] = meteo_dict["geoloc"]["lon"]
+        output = json.dumps(meteo_run_dict)
         return output
 
 def run(port = 8080):
